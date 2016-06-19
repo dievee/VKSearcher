b8fdb3e48bd40f066278a5a75202535f5646799e
dievee <nex7440@gmail.com>
2016-06-19 19:33:09 +0300

    Added identity

---
+++ VkSearcher/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace VkSearcher.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Year { get; set; } // place to add another properties of user
+        public ApplicationUser()
+        {
+
+        }
+    }
+}
